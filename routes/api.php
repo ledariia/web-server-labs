fa043202e432512bb2a19d97cb8ab7b46778a3c1
Dariia Lemekha <dariia.lemekha@oa.edu.ua>
2026-06-04 23:54:45 +0300

    Commit for lab8

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\Blog\PostController;
+// Підключаємо контролери адмінки
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
+use App\Http\Controllers\Api\Blog\Admin\PostController as AdminPostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('blog')->group(function () {
-    Route::apiResource('posts', PostController::class)->names('blog.posts');
-});
+// Адмінка
 Route::prefix('admin/blog')->group(function () {
     $methods = ['index', 'store', 'update'];
 
+    // Маршрути для категорій
     Route::apiResource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+
+    // Маршрути для постів (використовуємо AdminPostController)
+    Route::apiResource('posts', AdminPostController::class)
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
